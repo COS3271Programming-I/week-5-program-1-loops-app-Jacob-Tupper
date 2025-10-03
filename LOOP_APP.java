f0f027d0e792972e1989e2eeeafb72a83bcdfeb9
Jacob Tupper <122641100+Jacob-Tupper@users.noreply.github.com>
2025-10-03 16:17:45 -0500

    Add files via upload

---
+++ LOOP_APP.java
@@ -0,0 +1,10 @@
+package loop_app;
+
+public class LOOP_APP {
+	public static void main(String[] args){
+        int i;
+        for(i=1; i<5000; i=2*i+1){
+             System.out.println("Count is: " + i);
+        }
+   }
+}
